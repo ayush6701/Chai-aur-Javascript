0be5ca9b4f799c991563338ddc9ed5439821c457
ayush6701 <ayush20lv@gmail.com>
2026-04-06 03:07:03 +0530

    type of datatypes checked

--- .devcontainer/01_basics/test.js
+++ .devcontainer/01_basics/test.js
@@ -1,1 +1,40 @@
-console.log("qwerty")
+// checking the type of each datatype in javascript.
+
+// 1. Primitive datatype. 
+
+let myName = "Ayush";
+console.log(typeof myName);
+
+let myAge = 24;
+console.log(typeof myAge);
+
+let amMale = true;
+console.log(typeof amMale);
+
+let mySymbol = Symbol("SHOGUN");
+console.log(typeof mySymbol);
+
+let bigInt = 12345678910n;
+console.log(typeof bigInt);
+
+let isEmployed = null;
+console.log(isEmployed === null);
+
+let loveLife;
+console.log(typeof loveLife);
+
+// 2. non-Primitive datatype. 
+
+let myBikes = ["MT-09", "Z-900", "CBRRR-1000 Fireblade SP", "KATANA", "S-1000RR"];
+console.log(Array.isArray(myBikes));
+
+let myObject = {
+    myname: "Ayush",
+    myage: "24",
+    mygender: "male",
+    mycity: "bhopal"
+};
+console.log(typeof myObject);
+
+let myFunction = function(){};
+console.log(typeof myFunction);
